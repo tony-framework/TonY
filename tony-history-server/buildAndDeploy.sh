c8accb4695478f3c78370d5479485e513360e51e
Phat Dai Tran <phatdtran3k6@gmail.com>
2018-10-26 17:14:32 -0700

    Skeleton for Tony History Server (#29)
    * Skeleton for Tony History Server
    
    * Updated logo
    
    * Removed old code
    
    * Added build scripts, docs, assets, config files
    
    * Added main app + basic tests
    
    * Modified .gitignore to exclude files in tony-history-server
    
    * Updated .gitignore to ignore deploy.log
    
    * Updated docs + deploy script
    
    * Removed extra gradle bin + Updated build.gradle at root and ths
    
    * Removed clutter in routes config
    
    * Removed old Qihoo's files
    
    * Fixed indentation
    
    * Fixed indentation pt.2
    
    * Removed gradlew.bat
    
    * Updated indentation; Reverted back to original route name; Refactored code;
    
    * Modified Scala version
    
    * Cleaned up piping syntax for deploy script
    
    * Reverted script piping syntax; Modified indentation

---
+++ tony-history-server/buildAndDeploy.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+###########################################################################################################
+# NAME: buildAndDeploy.sh
+#
+# DESCRIPTION:
+# This script will send the playBinary.zip generated after running `gradle dist` to host and user given
+# given as inputs.
+#
+#
+# INPUT:
+# $1 - user
+# $2 - hostname
+#
+#
+# OUTPUT:
+# $DEPLOY_LOG: log file that includes all tasks.
+#
+#
+# ENVIRONMENT VARIABLES:
+#
+#
+# NOTES:
+# Run this script in tony-history-server/ folder.
+#
+#
+# EXIT CODE:
+# 0 - Success
+# 1 - Cleaning step failed
+# 2 - Bundling distribution zip step failed
+# 3 - Copying to remote host failed
+#
+#
+# CHANGELOG:
+# OCT 24 2018 PHAT TRAN
+############################################################################################################
+DEPLOY_LOG=deploy.log
+
+echo "Cleaning up old builds..." | tee $DEPLOY_LOG
+gradle clean 2>&1 | tee -a $DEPLOY_LOG
+if [ $? -ne 0 ];
+then
+  exit 1
+fi
+echo | tee -a $DEPLOY_LOG
+
+echo "Packaging distribution zip..." | tee -a $DEPLOY_LOG
+gradle dist 2>&1 | tee -a $DEPLOY_LOG
+if [ $? -ne 0 ];
+then
+  exit 2
+fi
+echo | tee -a $DEPLOY_LOG
+
+echo "Copying over to $1@$2..." | tee -a $DEPLOY_LOG
+scp build/distributions/playBinary.zip $1@$2:~/. 2>&1 | tee -a $DEPLOY_LOG
+if [ $? -ne 0 ];
+then
+  exit 3
+fi
+echo | tee -a $DEPLOY_LOG
+
+echo "Deployed to $1@$2!" | tee -a $DEPLOY_LOG
+exit 0
