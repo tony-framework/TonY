ae8ac5cccba90420b66d593ce58d236112393323
Phat Tran <phatdtran3k6@gmail.com>
2018-11-19 18:08:58 -0800

    Added / in between tonyHistoryFolder and jobId for safety

--- tony-history-server/app/controllers/JobConfigPageController.java
+++ tony-history-server/app/controllers/JobConfigPageController.java
@@ -4,11 +4,9 @@ import cache.CacheWrapper;
 import com.google.common.cache.Cache;
 import com.typesafe.config.Config;
 import hadoop.Configuration;
-import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 import models.JobConfig;
-import models.JobMetadata;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.HdfsConfiguration;
@@ -16,11 +14,9 @@ import play.Logger;
 import play.Logger.ALogger;
 import play.mvc.Controller;
 import play.mvc.Result;
+import utils.HdfsUtils;
 
 import static utils.ParserUtils.*;
-
-import hadoop.Security;
-import utils.HdfsUtils;
 
 
 public class JobConfigPageController extends Controller {
@@ -43,7 +39,7 @@ public class JobConfigPageController extends Controller {
 
     List<JobConfig> listOfConfigs;
     String tonyHistoryFolder = config.getString("tony.historyFolder");
-    Path xmlPath = new Path(tonyHistoryFolder + jobId + "/config.xml");
+    Path xmlPath = new Path(tonyHistoryFolder + "/" + jobId + "/config.xml");
     if (cache.asMap().containsKey(jobId)) {
       listOfConfigs = cache.getIfPresent(jobId);
       return ok(views.html.config.render(listOfConfigs));
