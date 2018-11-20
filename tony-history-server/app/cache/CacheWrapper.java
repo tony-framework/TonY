a5c1b63dd2715093a1bdb955f6d57bf6c197fa11
Phat Tran <phatdtran3k6@gmail.com>
2018-11-19 18:27:32 -0800

    Added docs for cache

--- tony-history-server/app/cache/CacheWrapper.java
+++ tony-history-server/app/cache/CacheWrapper.java
@@ -9,7 +9,19 @@ import models.JobMetadata;
 
 @Singleton
 public class CacheWrapper {
+  /**
+   * metadataCache
+   * - key: job ID (application_[0-9]+_[0-9]+)
+   * - value: JobMetadata object containing all the metadata of the jobs (user, status, etc.)
+   */
   private static Cache<String, JobMetadata> metadataCache;
+
+  /**
+   * configCache
+   * - key: job ID (application_[0-9]+_[0-9]+)
+   * - value: List of JobConfig objects. Each JobConfig object
+   * represents a {@code property} (name-val-source-final) in config.xml
+   */
   private static Cache<String, List<JobConfig>> configCache;
 
   public CacheWrapper() {
