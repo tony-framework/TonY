38cb1e06a6e2fa723db9565283fd61e6a46abc09
Keqiu Hu <khu@linkedin.com>
2018-12-17 11:57:01 -0800

    Fix a missing file name for python_venv_zip
    Add test to cover this
    
    Updated the wrong test script
    
    Create a new test resource file for handling the check of venv
    
    Fix a typo

---
+++ tony-core/src/test/resources/check_env_and_venv.py
@@ -0,0 +1,31 @@
+"""
+Copyright 2018 LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
+See LICENSE in the project root for license information.
+"""
+import time
+import logging
+import os
+import sys
+
+time.sleep(1)
+
+# Set up logging.
+log_root = logging.getLogger()
+log_root.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+log_root.addHandler(ch)
+
+if not os.path.isfile('test.zip'):
+    logging.error('test.zip doesn\'t exist')
+    exit(-1)
+if not os.path.isfile('venv/123.xml'):
+    logging.error('venv/123.xml doesn\'t exist')
+    exit(-1)
+
+if os.environ['ENV_CHECK'] == 'ENV_CHECK':
+    logging.info('Found ENV_CHECK environment variable.')
+    exit(0)
+else:
+    logging.error('Failed to find ENV_CHECK environment variable')
+    exit(1)
