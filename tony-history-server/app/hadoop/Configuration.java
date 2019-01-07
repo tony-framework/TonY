945bab7ebb87a395c92d326e4577236ce0bb190f
Anthony Hsu <ahsu@linkedin.com>
2019-01-07 11:55:30 -0800

    Fix TonY History Server ResourceManager link #153

--- tony-history-server/app/hadoop/Configuration.java
+++ tony-history-server/app/hadoop/Configuration.java
@@ -4,9 +4,11 @@ import java.io.File;
 import javax.inject.Singleton;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.HdfsConfiguration;
-import org.apache.hadoop.yarn.api.ApplicationConstants;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import play.Logger;
+
+import static org.apache.hadoop.yarn.api.ApplicationConstants.Environment.HADOOP_CONF_DIR;
+import static org.apache.hadoop.yarn.conf.YarnConfiguration.YARN_SITE_CONFIGURATION_FILE;
 
 
 /**
@@ -16,7 +18,6 @@ import play.Logger;
 public class Configuration {
   private static final Logger.ALogger LOG = Logger.of(Configuration.class);
 
-  private static final String HADOOP_CONF_DIR = ApplicationConstants.Environment.HADOOP_CONF_DIR.key();
   private static final String CORE_SITE_CONF = YarnConfiguration.CORE_SITE_CONFIGURATION_FILE;
   private static final String HDFS_SITE_CONF = "hdfs-site.xml";
 
@@ -25,16 +26,22 @@ public class Configuration {
 
   public Configuration() {
     hdfsConf = new HdfsConfiguration();
-    if (System.getenv("HADOOP_CONF_DIR") != null) {
-      hdfsConf.addResource(new Path(System.getenv(HADOOP_CONF_DIR) + File.separatorChar + CORE_SITE_CONF));
-      hdfsConf.addResource(new Path(System.getenv(HADOOP_CONF_DIR) + File.separatorChar + HDFS_SITE_CONF));
+    yarnConf = new YarnConfiguration();
+
+    String hadoopConfDir = System.getenv(HADOOP_CONF_DIR.key());
+    if (hadoopConfDir != null) {
+      Path coreSitePath = new Path(hadoopConfDir + File.separatorChar + CORE_SITE_CONF);
+
+      hdfsConf.addResource(coreSitePath);
+      hdfsConf.addResource(new Path(hadoopConfDir + File.separatorChar + HDFS_SITE_CONF));
+
+      yarnConf.addResource(coreSitePath);
+      yarnConf.addResource(new Path(hadoopConfDir + File.separatorChar + YARN_SITE_CONFIGURATION_FILE));
     }
 
     // return `kerberos` if on Kerberized cluster.
     // return `simple` if on unsecure cluster.
     LOG.info("Hadoop Auth Setting: " + hdfsConf.get("hadoop.security.authentication"));
-
-    yarnConf = new YarnConfiguration();
   }
 
   public HdfsConfiguration getHdfsConf() {
