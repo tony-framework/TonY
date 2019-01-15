3d60a60e505281033717562a9a8ef88e7cf844f0
Anthony Hsu <ahsu@linkedin.com>
2019-01-15 10:58:55 -0800

    Remove unused import

--- tony-cli/src/main/java/com/linkedin/tony/cli/ClusterSubmitter.java
+++ tony-cli/src/main/java/com/linkedin/tony/cli/ClusterSubmitter.java
@@ -20,7 +20,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 import static com.linkedin.tony.Constants.*;
 
