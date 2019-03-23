tony-framework/TonY

commit 819ef7ae4a20460f6ef5aeea3e3a64cc42e7a3b7

Add ClientCallbackHandler to pass back ApplicationId and TaskUrl (#207)
* Add ClientCallbackHandler to pass back ApplicationId and TaskUrl

* Make interface methods public

* Remove unused method

* Address comment

tony-cli/src/main/java/com/linkedin/tony/cli/NotebookSubmitter.java
@@ -4,6 +4,7 @@
  */
 package com.linkedin.tony.cli;
 
+import com.linkedin.tony.ClientCallbackHandler;
 import com.linkedin.tony.Constants;
 import com.linkedin.tony.TonyClient;
 import com.linkedin.tony.TonyConfigurationKeys;
@@ -15,6 +16,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.Set;
 import java.util.UUID;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
@@ -23,6 +25,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
 import org.apache.hadoop.yarn.exceptions.YarnException;
 
 import static com.linkedin.tony.Constants.*;
@@ -41,10 +44,11 @@ import static com.linkedin.tony.Constants.*;
  * CLASSPATH=$(${HADOOP_HDFS_HOME}/bin/hadoop classpath --glob):./:/home/khu/notebook/tony-cli-0.1.0-all.jar \
  * java com.linkedin.tony.cli.NotebookSubmitter --src_dir bin/ --executes "'bin/linotebook --ip=* $DISABLE_TOKEN'"
  */
-public class NotebookSubmitter extends TonySubmitter {
+public class NotebookSubmitter extends TonySubmitter implements ClientCallbackHandler {
   private static final Log LOG = LogFactory.getLog(NotebookSubmitter.class);
 
   private TonyClient client;
+  private Set<TaskUrl> taskUrlSet;
 
   private NotebookSubmitter() {
     this.client = new TonyClient(new Configuration());
@@ -91,8 +95,8 @@ public class NotebookSubmitter extends TonySubmitter {
     }));
     clientThread.start();
     while (clientThread.isAlive()) {
-      if (client.getTaskUrls() != null) {
-        for (TaskUrl taskUrl : client.getTaskUrls()) {
+      if (taskUrlSet != null) {
+        for (TaskUrl taskUrl : taskUrlSet) {
           if (taskUrl.getName().equals(Constants.NOTEBOOK_JOB_NAME)) {
             String[] hostPort = taskUrl.getUrl().split(":");
             ServerSocket localSocket = new ServerSocket(0);
@@ -121,4 +125,11 @@ public class NotebookSubmitter extends TonySubmitter {
     exitCode = submitter.submit(args);
     System.exit(exitCode);
   }
+
+  // ClientCallbackHandler callbacks
+  public void onApplicationIdReceived(ApplicationId appId) { }
+  public void onTaskUrlsReceived(Set<TaskUrl> taskUrlSet) {
+    this.taskUrlSet = taskUrlSet;
+  }
+
 }

tony-core/src/main/java/com/linkedin/tony/TonyClient.java
@@ -125,6 +125,8 @@ public class TonyClient implements AutoCloseable {
   private int hbInterval;
   private int maxHbMisses;
 
+  private ClientCallbackHandler callbackHandler;
+
   // For access from CLI.
   private Set<TaskUrl> taskUrls = new HashSet<>();
 
@@ -133,13 +135,14 @@ public class TonyClient implements AutoCloseable {
   }
 
   public TonyClient(Configuration conf) {
-    initOptions();
-    tonyConf = conf;
-    VersionInfo.injectVersionInfo(tonyConf);
+    this(null, conf);
   }
 
-  public ImmutableSet<TaskUrl> getTaskUrls() {
-    return ImmutableSet.copyOf(taskUrls);
+  public TonyClient(ClientCallbackHandler handler, Configuration conf) {
+    initOptions();
+    callbackHandler = handler;
+    tonyConf = conf;
+    VersionInfo.injectVersionInfo(tonyConf);
   }
 
   private boolean run() throws IOException, InterruptedException, URISyntaxException, YarnException {
@@ -165,6 +168,9 @@ public class TonyClient implements AutoCloseable {
     FileSystem fs = FileSystem.get(hdfsConf);
     ApplicationSubmissionContext appContext = app.getApplicationSubmissionContext();
     appId = appContext.getApplicationId();
+    if (callbackHandler != null) {
+      callbackHandler.onApplicationIdReceived(appId);
+    }
     appResourcesPath = new Path(fs.getHomeDirectory(), Constants.TONY_FOLDER + Path.SEPARATOR + appId.toString());
     if (srcDir != null) {
       if (Utils.isArchive(srcDir)) {
@@ -744,6 +750,9 @@ public class TonyClient implements AutoCloseable {
       if (amRpcServerInitialized && taskUrls.isEmpty()) {
         taskUrls = amRpcClient.getTaskUrls();
         if (!taskUrls.isEmpty()) {
+          if (callbackHandler != null) {
+            callbackHandler.onTaskUrlsReceived(ImmutableSet.copyOf(taskUrls));
+          }
           // Print TaskUrls
           new TreeSet<>(taskUrls).forEach(task -> Utils.printTaskUrl(task, LOG));
         }

tony-core/src/main/java/com/linkedin/tony/ClientCallbackHandler.java
@@ -1,0 +1,22 @@
+/**
+ * Copyright 2019 LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
+package com.linkedin.tony;
+
+import com.linkedin.tony.rpc.TaskUrl;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+
+import java.util.Set;
+
+/**
+ * ClientCallbackHandler is used to get callbacks from TonyClient when some
+ * asynchronous information becomes available.
+ */
+public interface ClientCallbackHandler {
+    // Called when TonyClient gets an application id response from RM.
+    public void onApplicationIdReceived(ApplicationId appId);
+
+    // Called when TonyClient gets a set of taskUrls from TonyAM.
+    public void onTaskUrlsReceived(Set<TaskUrl> taskUrlSet);
+}

commit 3609c6ae9a47a85036d92b546a24b056f8d024d2

Fix #218: NotebookSubmitter broken after #207

tony-cli/src/main/java/com/linkedin/tony/cli/ClusterSubmitter.java
@@ -19,6 +19,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.yarn.api.records.LocalResourceType;
+import org.apache.hadoop.yarn.exceptions.YarnException;
 
 import static com.linkedin.tony.Constants.*;
 
@@ -63,6 +64,14 @@ public class ClusterSubmitter extends TonySubmitter {
         LOG.error("Arguments failed to pass sanity check");
         return -1;
       }
+      // ensure application is killed when this process terminates
+      Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        try {
+          client.forceKillApplication();
+        } catch (YarnException | IOException e) {
+          LOG.error("Failed to kill application during shutdown.", e);
+        }
+      }));
       exitCode = client.start();
     } catch (IOException e) {
       LOG.fatal("Failed to create FileSystem: ", e);

tony-cli/src/main/java/com/linkedin/tony/cli/NotebookSubmitter.java
@@ -8,17 +8,16 @@ import com.linkedin.tony.ClientCallbackHandler;
 import com.linkedin.tony.Constants;
 import com.linkedin.tony.TonyClient;
 import com.linkedin.tony.TonyConfigurationKeys;
-import com.linkedin.tony.util.Utils;
 import com.linkedin.tony.rpc.TaskUrl;
 import com.linkedin.tonyproxy.ProxyServer;
 import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.UUID;
-import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -44,24 +43,37 @@ import static com.linkedin.tony.Constants.*;
  * CLASSPATH=$(${HADOOP_HDFS_HOME}/bin/hadoop classpath --glob):./:/home/khu/notebook/tony-cli-0.1.0-all.jar \
  * java com.linkedin.tony.cli.NotebookSubmitter --src_dir bin/ --executes "'bin/linotebook --ip=* $DISABLE_TOKEN'"
  */
-public class NotebookSubmitter extends TonySubmitter implements ClientCallbackHandler {
+public class NotebookSubmitter extends TonySubmitter {
   private static final Log LOG = LogFactory.getLog(NotebookSubmitter.class);
 
-  private TonyClient client;
-  private Set<TaskUrl> taskUrlSet;
+  private static class NotebookCallbackHandler implements ClientCallbackHandler {
+    private Set<TaskUrl> taskUrlSet;
 
-  private NotebookSubmitter() {
-    this.client = new TonyClient(new Configuration());
+    Set<TaskUrl> getTaskUrls() {
+      return taskUrlSet;
+    }
+
+    @Override
+    public void onApplicationIdReceived(ApplicationId appId) { }
+
+    @Override
+    public void onTaskUrlsReceived(Set<TaskUrl> taskUrls) {
+      taskUrlSet = taskUrls;
+    }
+  }
+
+  private NotebookCallbackHandler callbackHandler;
+  private TonyClient client;
+
+  public NotebookSubmitter() {
+    callbackHandler = new NotebookCallbackHandler();
+    client = new TonyClient(callbackHandler, new Configuration());
   }
 
   public int submit(String[] args)
-      throws ParseException, URISyntaxException, IOException, InterruptedException, YarnException {
+      throws ParseException, URISyntaxException, IOException, InterruptedException {
     LOG.info("Starting NotebookSubmitter..");
     String jarPath = new File(NotebookSubmitter.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
-    Options opts = Utils.getCommonOptions();
-    opts.addOption("conf", true, "User specified configuration, as key=val pairs");
-    opts.addOption("conf_file", true, "Name of user specified conf file, on the classpath");
-    opts.addOption("src_dir", true, "Name of directory of source files.");
 
     int exitCode = 0;
     Path cachedLibPath;
@@ -80,7 +92,7 @@ public class NotebookSubmitter extends TonySubmitter implements ClientCallbackHa
     updatedArgs[args.length] = "--hdfs_classpath";
     updatedArgs[args.length + 1] = cachedLibPath.toString();
     updatedArgs[args.length + 2] = "--conf";
-    updatedArgs[args.length + 3] = TonyConfigurationKeys.APPLICATION_TIMEOUT + "=" + String.valueOf(24 * 60 * 60 * 1000);
+    updatedArgs[args.length + 3] = TonyConfigurationKeys.APPLICATION_TIMEOUT + "=" + (24 * 60 * 60 * 1000);
 
     client.init(updatedArgs);
     Thread clientThread = new Thread(client::start);
@@ -95,14 +107,16 @@ public class NotebookSubmitter extends TonySubmitter implements ClientCallbackHa
     }));
     clientThread.start();
     while (clientThread.isAlive()) {
-      if (taskUrlSet != null) {
-        for (TaskUrl taskUrl : taskUrlSet) {
+      if (callbackHandler.getTaskUrls() != null) {
+        for (TaskUrl taskUrl : callbackHandler.getTaskUrls()) {
           if (taskUrl.getName().equals(Constants.NOTEBOOK_JOB_NAME)) {
-            String[] hostPort = taskUrl.getUrl().split(":");
+            URL url = new URL(taskUrl.getUrl());
+            String host = url.getHost();
+            int port = url.getPort();
             ServerSocket localSocket = new ServerSocket(0);
             int localPort = localSocket.getLocalPort();
             localSocket.close();
-            ProxyServer server = new ProxyServer(hostPort[0], Integer.parseInt(hostPort[1]), localPort);
+            ProxyServer server = new ProxyServer(host, port, localPort);
             LOG.info("If you are running NotebookSubmitter in your local box, please open [localhost:" + localPort
                 + "] in your browser to visit the page. Otherwise, if you're running NotebookSubmitter in a remote "
                 + "machine (like a gateway), please run" + " [ssh -L 18888:localhost:" + localPort
@@ -119,17 +133,14 @@ public class NotebookSubmitter extends TonySubmitter implements ClientCallbackHa
     return exitCode;
   }
 
+  public TonyClient getClient() {
+    return client;
+  }
+
   public static void main(String[] args) throws  Exception {
     int exitCode;
     NotebookSubmitter submitter = new NotebookSubmitter();
     exitCode = submitter.submit(args);
     System.exit(exitCode);
   }
-
-  // ClientCallbackHandler callbacks
-  public void onApplicationIdReceived(ApplicationId appId) { }
-  public void onTaskUrlsReceived(Set<TaskUrl> taskUrlSet) {
-    this.taskUrlSet = taskUrlSet;
-  }
-
 }

tony-core/src/main/java/com/linkedin/tony/TonyApplicationMaster.java
@@ -701,8 +701,8 @@ public class TonyApplicationMaster {
       int tbPort = tbSocket.getLocalPort();
       extraEnv.put(Constants.TB_PORT, String.valueOf(tbPort));
       String tbUrl = Utils.getCurrentHostName() + ":" + tbPort;
-      proxyUrl = tbUrl;
-      LOG.info("Registering tensorboard url for single node training: " + tbUrl);
+      proxyUrl = Utils.constructUrl(tbUrl);
+      LOG.info("Registering TensorBoard url for single node training: " + tbUrl);
       registerTensorBoardUrlToRM(tbUrl);
       tbSocket.close();
     }

tony-core/src/main/java/com/linkedin/tony/TonyClient.java
@@ -825,7 +825,10 @@ public class TonyClient implements AutoCloseable {
     if (appId != null) {
       yarnClient.killApplication(appId);
     }
+  }
 
+  public ClientCallbackHandler getCallbackHandler() {
+    return callbackHandler;
   }
 
   protected ApplicationRpcClient getAMRpcClient() {

tony-core/src/main/java/com/linkedin/tony/util/Utils.java
@@ -173,13 +173,15 @@ public class Utils {
     resource.setResourceValue(GPU_URI, gpuCount);
   }
 
-  public static String constructContainerUrl(Container container) {
-    try {
-      return String.format(WORKER_LOG_URL_TEMPLATE, container.getNodeHttpAddress(), container.getId(),
-          UserGroupInformation.getCurrentUser().getShortUserName());
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+  public static String constructUrl(String urlString) {
+    if (!urlString.startsWith("http")) {
+      return "http://" + urlString;
     }
+    return urlString;
+  }
+
+  public static String constructContainerUrl(Container container) {
+    return constructContainerUrl(container.getNodeHttpAddress(), container.getId());
   }
 
   public static String constructContainerUrl(String nodeAddress, ContainerId containerId) {

tony-cli/src/test/java/com/linkedin/tony/cli/TestClusterSubmitter.java
@@ -2,9 +2,11 @@
  * Copyright 2018 LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
  * See LICENSE in the project root for license information.
  */
-package com.linkedin.tony;
+package com.linkedin.tony.cli;
 
-import com.linkedin.tony.cli.ClusterSubmitter;
+import com.linkedin.tony.Constants;
+import com.linkedin.tony.TonyClient;
+import com.linkedin.tony.TonyConfigurationKeys;
 import org.testng.annotations.Test;
 
 import static org.mockito.Mockito.*;

tony-cli/src/test/java/com/linkedin/tony/cli/TestNotebookSubmitter.java
@@ -1,0 +1,19 @@
+/**
+ * Copyright 2019 LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
+package com.linkedin.tony.cli;
+
+import com.linkedin.tony.TonyClient;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestNotebookSubmitter {
+  @Test
+  public void testCallbackHandlerSet() {
+    NotebookSubmitter ns = new NotebookSubmitter();
+    TonyClient client = ns.getClient();
+    Assert.assertNotNull(client.getCallbackHandler());
+  }
+}