6202947a63e5e1b4914e3806e56b6ca8ff6ac811
Anthony Hsu <ahsu@linkedin.com>
2019-03-26 11:45:37 -0700

    Change TonY to TONY

--- tony-core/src/main/java/com/linkedin/tony/TonyClient.java
+++ tony-core/src/main/java/com/linkedin/tony/TonyClient.java
@@ -91,7 +91,7 @@ import org.apache.hadoop.yarn.util.Records;
 public class TonyClient implements AutoCloseable {
   private static final Log LOG = LogFactory.getLog(TonyClient.class);
 
-  private static final String APP_TYPE = "TonY";
+  private static final String APP_TYPE = "TONY";
 
   // Configurations
   private YarnClient yarnClient;
