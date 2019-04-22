77e48f6beb5e61eff46e318b0b658c5c8a2cc35e
Anthony Hsu <ahsu@linkedin.com>
2019-04-22 08:40:29 -0700

    Fix ./gradlew :tony-core:licenseMain

--- tony-core/src/main/java/com/linkedin/tony/TaskMonitor.java
+++ tony-core/src/main/java/com/linkedin/tony/TaskMonitor.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2019 LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
 package com.linkedin.tony;
 
 import com.linkedin.tony.rpc.MetricWritable;
