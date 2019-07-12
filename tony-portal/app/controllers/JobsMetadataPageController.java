0cbdb64289341cfab7a9faa6babeb27d968cd5a5
ionyeneho <ionyeneho@linkedin.com>
2019-07-11 23:05:13 -0700

    *Creates sorted table of TonY records*
    When viewing the TonY Portal page, the records are displayed in a random order. This fix displays TonY records sorted by most recent process completion date.

--- tony-portal/app/controllers/JobsMetadataPageController.java
+++ tony-portal/app/controllers/JobsMetadataPageController.java
@@ -3,6 +3,8 @@ package controllers;
 import cache.CacheWrapper;
 import com.google.common.cache.Cache;
 import com.linkedin.tony.models.JobMetadata;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -16,7 +18,15 @@ public class JobsMetadataPageController extends Controller {
     cache = cacheWrapper.getMetadataCache();
   }
 
+
   public Result index() {
-    return ok(views.html.metadata.render(cache.asMap().values()));
+
+    return ok(views.html.metadata.render(cache.asMap().values()
+        .stream()
+        .sorted(Comparator.comparingLong(JobMetadata::getCompleted).reversed())
+        .collect(Collectors.toList())));
   }
 }
+
+
+
