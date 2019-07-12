3bb5aa6d8f5a1e851ebe46ac41c4514e6b856e5d
ionyeneho <ionyeneho@linkedin.com>
2019-07-12 15:31:39 -0700

    *Updated toString() function to remove new lines *
    In my original toString() function, I included new lines in
    the returned string to better display the output.
    It’s preferable in this case to retain the default output and
    allow the user to construct their own custum string.
    Also in this change, I  perform a minor fix to merge the
    first two string segments of the output where before
    they were separate strings.

--- tony-core/src/main/java/com/linkedin/tony/models/JobMetadata.java
+++ tony-core/src/main/java/com/linkedin/tony/models/JobMetadata.java
@@ -137,8 +137,8 @@ public class JobMetadata {
 
   @Override
   public String toString() {
-    return "\nMetadata{" + "id: " + id + ",\n joblink: " + jobLink + ",\n configlink: " + configLink + ",\n rmLink: "
-        + rmLink + ",\n dateStarted: " + new Date(started) + ",\n started: " + started
-        + ",\n dateCompleted: " + new Date(completed) + ",\n status: " + status + ",\n user: " + user + "}";
+    return "Metadata{ id: " + id + ", joblink: " + jobLink + ", configlink: " + configLink + ", rmLink: "
+        + rmLink + ", dateStarted: " + new Date(started) + ", started: " + started
+        + ", dateCompleted: " + new Date(completed) + ", status: " + status + ", user: " + user + "}";
   }
 }
