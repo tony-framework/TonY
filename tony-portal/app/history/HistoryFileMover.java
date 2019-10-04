4ae27cb368fbf0da3911798b24d12339dfa4af24
ionyeneho <ionyeneho@linkedin.com>
2019-10-04 13:47:06 -0700

    *Updates the .jhist File Path for Killed Applications*
    When an application is killed by the user, there is no opportunity for a graceful shutdown and those files are never renamed from .jhist.inprogress to the final application status such as  -FAILED.jhist or -SUCCEEDED.jhist causing those applications to be interpreted by TonY as currently RUNNING on TonY portal even though they have been terminated.  This fix renames those files to -KILLED.jhist, correctly identifying the final status of those killed applications.

--- tony-portal/app/history/HistoryFileMover.java
+++ tony-portal/app/history/HistoryFileMover.java
@@ -7,17 +7,27 @@ import com.linkedin.tony.util.ParserUtils;
 import com.linkedin.tony.util.Utils;
 import com.typesafe.config.Config;
 import hadoop.Requirements;
+import java.io.File;
 import java.io.IOException;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.EnumSet;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.yarn.api.records.ApplicationReport;
+import org.apache.hadoop.yarn.api.records.YarnApplicationState;
+import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.exceptions.YarnException;
 import play.Logger;
 import utils.ConfigUtils;
 
@@ -65,6 +75,48 @@ public class HistoryFileMover {
         }
       }
     }, 0, moverIntervalMs, TimeUnit.MILLISECONDS);
+
+    //Compares the Yarn appIDs of Killed apps to those in the
+    // intermediate directory and calls a function to rename them
+
+    scheduledThreadPool.scheduleAtFixedRate(() -> {
+      FileStatus[] intermedDir = null;
+      List<FileStatus> killedDirsList = new ArrayList<>();
+
+      try {
+        intermedDir = fs.listStatus(intermediateDir);
+      } catch (IOException e) {
+        LOG.error("Failed to list files in " + intermediateDir , e);
+      }
+
+      if (intermedDir != null) {
+
+        try {
+          for (String k : getKilledApps()) {
+            for (FileStatus jobDr : intermedDir) {
+              if (jobDr.toString().contains(k)) {
+                killedDirsList.add(jobDr);
+              }
+            }
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+        } catch (YarnException e) {
+          e.printStackTrace();
+        }
+
+        LOG.info("Number of Killed Apps in Intermediate Directory: " + killedDirsList.size());
+
+        try {
+          renameKilledApps(fs, killedDirsList);
+          LOG.info("Renamed Killed Apps in Intermediate Directory");
+        } catch (Exception e) {
+          LOG.error("Encountered exception renaming KILLED apps", e);
+        }
+      }
+
+    }, 0, moverIntervalMs, TimeUnit.MILLISECONDS);
+
   }
 
   private void moveIntermediateToFinished(FileSystem fs, FileStatus[] jobDirs, ZoneId zoneId) {
@@ -98,5 +150,66 @@ public class HistoryFileMover {
 
   private boolean jobInProgress(String jhistFileName) {
     return !jhistFileName.endsWith(Constants.HISTFILE_SUFFIX);
+  }
+
+  // This function queries and returns Yarn for the App IDs of apps with YarnApplicationState.KILLED
+
+  private List<String> getKilledApps() throws IOException, YarnException {
+    List<String> totalKilledAppIds = new ArrayList<String>();
+    long currentTimestamp = System.currentTimeMillis();
+
+    //set timeDelta for 36 hours
+    long timeDelta = 36 * 60 * 60 * 1000;
+    YarnConfiguration yarnConf = new YarnConfiguration();
+    if (System.getenv(Constants.HADOOP_CONF_DIR) != null) {
+      yarnConf.addResource(new Path(System.getenv(Constants.HADOOP_CONF_DIR) +
+          File.separatorChar + Constants.CORE_SITE_CONF));
+      yarnConf.addResource(new Path(System.getenv(Constants.HADOOP_CONF_DIR) +
+          File.separatorChar + Constants.YARN_SITE_CONF));
+    }
+    YarnClient yarnClient = YarnClient.createYarnClient();
+    yarnClient.init(yarnConf);
+    yarnClient.start();
+    List<ApplicationReport> totalKilledApps = yarnClient.getApplications(EnumSet.of(YarnApplicationState.KILLED));
+
+    // Get apps within a specified time period. This prevents the History File mover from iterating through
+    //previously checked killed app IDs upon every iteration.
+
+    List<String> timeDeltaKilledIds = totalKilledApps
+        .stream()
+        .filter(x -> x.getStartTime() > (currentTimestamp - timeDelta))
+        .map(x -> x.getApplicationId().toString())
+        .collect(Collectors.toList());
+
+    LOG.info("Number Killed Yarn Apps within timeDelta: " + timeDeltaKilledIds.size());
+    return timeDeltaKilledIds;
+  }
+
+  private void renameKilledApps(FileSystem fs, List<FileStatus> jobDirs) {
+
+    for (FileStatus jobDir : jobDirs) {
+      String jhistFilePath = ParserUtils.getJhistFilePath(fs, jobDir.getPath());
+
+      if (jhistFilePath.endsWith(".jhist.inprogress")) {
+        {
+          //new file name will need an end time, set it to current time
+          long currentTimestamp = System.currentTimeMillis();
+          Path sourcePath = new Path(jhistFilePath);
+          String jhistFileName = jhistFilePath.substring(jhistFilePath.lastIndexOf('/') + 1);
+
+          //Section of filename that will be replaced --> username.jhist.inprogress
+          String oldJhistSubstring = jhistFileName.substring(jhistFileName.lastIndexOf('-') + 1);
+          String username = oldJhistSubstring.split("\\.")[0];
+          String newJhistSubstring = currentTimestamp + "-" + username + "-KILLED.jhist";
+          String killedJhistFilePath = jhistFilePath.replace(oldJhistSubstring, newJhistSubstring);
+          Path killedPath = new Path(killedJhistFilePath);
+          try {
+            fs.rename(sourcePath, killedPath);
+          } catch (IOException e) {
+            LOG.error("Failed to rename KILLED apps", e);
+          }
+        }
+      }
+    }
   }
 }
