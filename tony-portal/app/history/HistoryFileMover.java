c2d98692778f665e337f993ca49c2357ec6c43b4
Pralabh Kumar <pkumar2@linkedin.com>
2020-01-31 09:58:33 +0530

    Pushing back original configuration files

--- tony-portal/app/history/HistoryFileMover.java
+++ tony-portal/app/history/HistoryFileMover.java
@@ -60,7 +60,6 @@ public class HistoryFileMover {
 
     YarnConfiguration yarnConf = new YarnConfiguration();
     if (System.getenv(Constants.HADOOP_CONF_DIR) != null) {
-      LOG.info(" Path to hadoop configuration is null ");
       yarnConf.addResource(new Path(System.getenv(Constants.HADOOP_CONF_DIR) +
           File.separatorChar + Constants.CORE_SITE_CONF));
       yarnConf.addResource(new Path(System.getenv(Constants.HADOOP_CONF_DIR) +
