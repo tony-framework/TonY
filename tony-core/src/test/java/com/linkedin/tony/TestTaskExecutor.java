ae09966f302be2c0eea9ccd87824589c67cb91db
Cheng Ren <1428327+chengren311@users.noreply.github.com>
2020-07-06 23:58:22 -0700

    in progress

--- tony-core/src/test/java/com/linkedin/tony/TestTaskExecutor.java
+++ tony-core/src/test/java/com/linkedin/tony/TestTaskExecutor.java
@@ -50,7 +50,7 @@ public class TestTaskExecutor {
   @Test
   public void testPortRangeIsValid() {
     Assert.assertEquals(TaskExecutor.PORT_RANGE.getMinimum().intValue(), 1);
-    Assert.assertEquals(TaskExecutor.PORT_RANGE.getMaximum().intValue(), 65536);
+    Assert.assertEquals(TaskExecutor.PORT_RANGE.getMaximum().intValue(), 65535);
   }
 
   @Test
