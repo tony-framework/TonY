be8d7c84d37a881d850dc92213f654f62388f58a
Christian Gügi <christian.guegi@scigility.com>
2020-11-25 10:18:19 +0100

    enabled memory growth for physical device

--- tony-examples/mnist-tensorflow/mnist_keras_distributed.py
+++ tony-examples/mnist-tensorflow/mnist_keras_distributed.py
@@ -79,4 +79,10 @@ def train_mnist():
 
 
 if __name__ == '__main__':
+    physical_devices = tf.config.list_physical_devices('GPU')
+    try:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    except:
+        # Invalid device or cannot modify virtual devices once initialized.
+        pass
     train_mnist()
