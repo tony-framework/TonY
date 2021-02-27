c7407dba3e1bf94b3fbd02fb279942316cc5d81e
junfan.zhang <zuston.shacha@gmail.com>
2021-02-27 16:16:01 +0800

    Evaluator should be standalone with training cluster in TF (#512)
    * In TF, remove training cluster role config from TF_CONFIG when task name is evaluator
    
    * Fix code style failed
    
    * Remove eval when role is ps/worker/chief
    
    * Add more tests
    
    * To compatible with tf estimator, reserve cluster spec into TF_CONFIG in eval role
    
    * Keep consistent with ps or eval role

--- tony-core/src/test/java/com/linkedin/tony/util/TestUtils.java
+++ tony-core/src/test/java/com/linkedin/tony/util/TestUtils.java
@@ -10,6 +10,7 @@ import com.google.common.collect.ImmutableMap;
 import com.linkedin.tony.TFConfig;
 import com.linkedin.tony.TonyConfigurationKeys;
 import com.linkedin.tony.tensorflow.JobContainerRequest;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.SocketException;
@@ -22,6 +23,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.TreeMap;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.CommonConfigurationKeys;
 import org.apache.hadoop.yarn.api.records.Container;
@@ -32,8 +34,8 @@ import org.apache.hadoop.yarn.exceptions.YarnException;
 import org.apache.hadoop.yarn.util.resource.ResourceUtils;
 import org.testng.annotations.Test;
 
-import static com.linkedin.tony.Constants.LOGS_SUFFIX;
 import static com.linkedin.tony.Constants.JOBS_SUFFIX;
+import static com.linkedin.tony.Constants.LOGS_SUFFIX;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
@@ -158,6 +160,20 @@ public class TestUtils {
     TFConfig config = mapper.readValue(tfConfig, new TypeReference<TFConfig>() { });
     assertEquals(config.getTask().getType(), "worker");
     assertEquals(config.getTask().getIndex(), 1);
+    assertEquals(config.getCluster().get("worker").get(0), "host0:1234");
+    assertEquals(config.getCluster().get("worker").get(1), "host1:1234");
+    assertEquals(config.getCluster().get("ps").get(0), "host2:1234");
+
+    spec = "{\"worker\":[\"host0:1234\", \"host1:1234\"], "
+            + "\"ps\":[\"host2:1234\"],\"evaluator\":[\"host3:1234\"]}";
+    tfConfig = Utils.constructTFConfig(spec, "worker", 0);
+    config = mapper.readValue(tfConfig, new TypeReference<TFConfig>() { });
+    assertEquals(config.getTask().getType(), "worker");
+    assertEquals(config.getTask().getIndex(), 0);
+    assertEquals(config.getCluster().size(), 2);
+    assertNotNull(config.getCluster().get("ps"));
+    assertNotNull(config.getCluster().get("worker"));
+    assertNull(config.getCluster().get("evaluator"));
     assertEquals(config.getCluster().get("worker").get(0), "host0:1234");
     assertEquals(config.getCluster().get("worker").get(1), "host1:1234");
     assertEquals(config.getCluster().get("ps").get(0), "host2:1234");
