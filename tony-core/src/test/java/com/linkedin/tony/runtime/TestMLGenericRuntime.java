5926b0f5cc797e02eeec79fbbff8578caf07fa94
zhangjunfan <junfan.zhang@outlook.com>
2021-11-28 14:05:15 +0800

    Fix compile error
    Signed-off-by: zhangjunfan <junfan.zhang@outlook.com>

--- tony-core/src/test/java/com/linkedin/tony/runtime/TestMLGenericRuntime.java
+++ tony-core/src/test/java/com/linkedin/tony/runtime/TestMLGenericRuntime.java
@@ -16,7 +16,6 @@
 package com.linkedin.tony.runtime;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.yarn.api.records.CollectorInfo;
 import org.apache.hadoop.yarn.api.records.ContainerExitStatus;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
